36098ed668471b6071eaf5ffd8b036790490ee96
R Yang <ray_yang@mail.fresnostate.edu>
2021-05-09 22:08:58 -0700

    exam end code

---
+++ src/app/users/user.service.ts
@@ -0,0 +1,57 @@
+import { Component, Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class UserService {
+
+    private apiUrl: string;
+
+    constructor(
+        private http: Http
+    ) {
+        this.apiUrl = environment.apiUrl;
+    }
+
+    getUsers(): Promise<Array<Object>> {
+        return this.http.get(`${this.apiUrl}/user`)
+            .toPromise()
+            .then((resp) => {
+                let users = resp.json();
+                return users;
+            });
+    }
+
+    getUserById(userId): Promise<Object> {
+        return this.http.get(`${this.apiUrl}/user/id/${userId}`)
+            .toPromise()
+            .then((resp) => {
+                return resp.json();
+            });
+    }
+
+    addUser(user): Promise<Object> {
+        return this.http.post(`${this.apiUrl}/user`, user)
+            .toPromise()
+            .then((resp) => {
+                return resp.json();
+            });
+    }
+
+    deleteUser(id): Promise<Object> {
+        return this.http.delete(`${this.apiUrl}/user/id/${id}`)
+            .toPromise()
+            .then((resp) => {
+                return resp.json();
+            });
+    }
+
+    updateUser(id, user): Promise<Object> {
+        return this.http.put(`${this.apiUrl}/user/id/${id}`, user)
+            .toPromise()
+            .then((resp) => {
+                return resp.json();
+            });
+    }
+
+}
